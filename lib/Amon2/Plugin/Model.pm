67e9365da6128817c38ef7007ef419e22e4f3389
issm <issmxx@gmail.com>
2011-12-30 00:35:42 +0900

    initial commit

---
+++ lib/Amon2/Plugin/Model.pm
@@ -0,0 +1,32 @@
+package Amon2::Plugin::Model;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Amon2::Plugin::Model -
+
+=head1 SYNOPSIS
+
+  use Amon2::Plugin::Model;
+
+=head1 DESCRIPTION
+
+Amon2::Plugin::Model is
+
+=head1 AUTHOR
+
+issm E<lt>issmxx@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
